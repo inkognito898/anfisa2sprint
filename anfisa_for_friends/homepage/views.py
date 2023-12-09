55eea6aa91e524474900aae76d09cae93d19fca6
Валерий Валевко <valevko.v@yandex.ru>
2023-12-09 21:33:12 +0300

    venv issue

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.db.models import Q
+from ice_cream.models import IceCream
 
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    # Запишите в переменную ice_cream_list новый QuerySet
+    ice_cream_list = IceCream.objects.values(
+        'id',
+        'title',
+        'description',
+        'price'
+        ).filter(Q(is_published=True) & Q(is_on_main=True) & Q(category__is_published=True))
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template, context)
--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Из модуля models импортируем модель Category...
+from .models import Category, Topping, Wrapper, IceCream
+
+admin.site.empty_value_display = 'Не задано'
+
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    filter_horizontal = ('toppings',)
+
+
+class IceCreamInline(admin.StackedInline):
+    model = IceCream
+    extra = 0
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInline,
+    )
+    list_display = (
+        'title',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(IceCream, IceCreamAdmin)
+admin.site.register(Topping)
+admin.site.register(Wrapper)
